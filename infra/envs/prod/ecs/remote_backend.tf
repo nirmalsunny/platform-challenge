4d6f7a5db738a75c062c3db29287ef0c2ae0479f
Nirmal Sunny <nirmal.sunny@arm.com>
2023-02-03 17:26:46 +0000

    Renamed accounts to envs

--- infra/envs/prod/ecs/remote_backend.tf
+++ infra/envs/prod/ecs/remote_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "platform-challenge-terraform-state"
-    key            = "${var.env}/ecs/terraform.tfstate"
+    key            = "prod/ecs/terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "platform-challenge-state-locks"
     encrypt        = true
