f08e5bb1cac090a29454a9a5199712b3da1fd61c
Nirmal Sunny <nirmal.sunny@arm.com>
2023-02-06 15:11:07 +0000

    only env url needs to be output

--- infra/envs/dev/ecs/out.tf
+++ infra/envs/dev/ecs/out.tf
@@ -2,8 +2,3 @@ output "web_endpoint" {
   value       = "http://${aws_lb.platform_dev.dns_name}"
   description = "hit this url to access web server"
 }
-
-output "ecr_repo_url" {
-  value       = aws_ecr_repository.platform_challenge.repository_url
-  description = "url where the docker image is to be pushed"
-}
